0463de7f1d7d99507a78938a7c49cebc5870bcdc
Kuldeep Pandey <84696370+Kuldeep525@users.noreply.github.com>
2021-08-07 20:59:29 +0530

    Update Problem_with_function.js

--- Problem_with_function.js
+++ Problem_with_function.js
@@ -70,12 +70,6 @@ function checkIsPrime(number){
     }
 }
 
-// Problem 3 : Take a number from user and check if the number is a Prime then show
-//             that its palindrome is also prime
-//             a. Write function check if number is Prime
-//             b. Write function to get the Palindrome.
-//             c. Check if the Palindrome number is also prime
-
 function checkIsPalidrome(number){
    let reverse = 0;
    let remainder = 0;
